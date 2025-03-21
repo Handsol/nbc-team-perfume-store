1c5efbb4a75f4783330a61d6bd9614f1ad15f7a6
lee kiri <mango.man.e.ya@gmail.com>
2025-03-21 17:28:10 +0900

    feat#4 Supabase 클라이언트 초기화 로직 개선(중복 인스턴스 방지)

--- src/utils/supabase/createClient.ts
+++ src/utils/supabase/createClient.ts
@@ -1,7 +1,10 @@
 import { isServer } from '@tanstack/react-query';
-import { createBrowserClient } from '@supabase/ssr';
-import { createServerClient } from '@supabase/ssr';
+import { createBrowserClient, createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
+import { SupabaseClient } from '@supabase/supabase-js';
+
+let browserClient: SupabaseClient | null = null;
+let serverClient: SupabaseClient | null = null;
 
 export const createClient = () => {
   const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
@@ -9,27 +12,27 @@ export const createClient = () => {
 
   //클라이언트
   if (!isServer) {
-    const browserClient = createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+    if (!browserClient) {
+      browserClient = createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+    }
     return browserClient;
   }
 
   //서버
-  const cookieStore = cookies();
-  const serverClient = createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll();
-      },
-      setAll(cookiesToSet) {
-        try {
-          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
-        } catch {
-          // The `setAll` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
+  if (!serverClient) {
+    const cookieStore = cookies();
+    serverClient = createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+          } catch {}
         }
       }
-    }
-  });
+    });
+  }
   return serverClient;
 };
