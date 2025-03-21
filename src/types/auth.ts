b829718c03fedc79c24e421413b7d1477d9ff5e1
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-21 20:40:24 +0900

    add #10 - 인증상태 관리 authStore에서 사용하는 type 정의

---
+++ src/types/auth.ts
@@ -0,0 +1,15 @@
+export interface User {
+  id: string;
+  nickname: string;
+  email: string;
+  created_at: string;
+}
+
+export interface AuthState {
+  isLogin: boolean;
+  user: User;
+  token: string | null;
+  setLogin: (user: User, token: string) => void;
+  setLogout: () => void;
+  setToken: (token: string) => void;
+}
