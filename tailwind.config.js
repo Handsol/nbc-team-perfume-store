b3c168a4c2ba3be3f0224bdd59626e3f435de63e
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-21 15:39:45 +0900

    chore #3 - turbopack 에러 해결

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
@@ -50,3 +50,5 @@ export default {
   },
   plugins: []
 };
+
+export default config;
