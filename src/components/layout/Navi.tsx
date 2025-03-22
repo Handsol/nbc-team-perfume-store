e8f6450c7c6f47f7c30312cce2c318efc3faa035
Handsol <imhandsol@gmail.com>
2025-03-22 21:16:27 +0900

    style #9 - 텍스트 색상 변경

--- src/components/layout/Navi.tsx
+++ src/components/layout/Navi.tsx
@@ -4,8 +4,8 @@ const Navi = () => {
       <div className="flex w-full max-w-[1000px] h-[50px] mx-auto divide-x divide-[#d3d3d3]">
         {/* 남성용 */}
         <div className="flex flex-row flex-1 items-center px-4 gap-5 min-w-0">
-          <button className="font-semibold whitespace-nowrap">For Man</button>
-          <div className="flex gap-6 flex-wrap shrink min-w-0">
+          <button className="font-semibold whitespace-nowrap text-black">For Man</button>
+          <div className="flex gap-6 flex-wrap shrink min-w-0 text-l text-gray">
             <button>오 드 뚜왈렛</button>
             <button>오 드 퍼퓸</button>
             <button>코롱</button>
@@ -15,8 +15,8 @@ const Navi = () => {
 
         {/* 여성용 */}
         <div className="flex flex-row flex-1 items-center px-4 gap-5 min-w-0">
-          <button className="font-semibold whitespace-nowrap">For Woman</button>
-          <div className="flex gap-6 flex-wrap shrink min-w-0">
+          <button className="font-semibold whitespace-nowrap text-black">For Woman</button>
+          <div className="flex gap-6 flex-wrap shrink min-w-0 text-l text-gray">
             <button>오 드 뚜왈렛</button>
             <button>오 드 퍼퓸</button>
             <button>코롱</button>
