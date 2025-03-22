75e9f254f04eb0ecce793ad9940209894617b8f2
Handsol <imhandsol@gmail.com>
2025-03-22 18:25:50 +0900

    feat #9 - 헤더에 검색 컴포넌트 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,22 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Search from '../Search';
 
 const Header = () => {
   return (
-    <div>
-      <div className="flex flex-row justify-between items-center px-[250px] w-full h-[100px] ">
+    <div className="flex flex-col items-center w-full border-b border-lightgray">
+      <div className="flex flex-row w-full max-w-[1400px] h-[100px] justify-between items-center px-4">
         <Link href={'/'}>
           <Image src={'/orot-logo.png'} alt="orot logo" width={150} height={0} />
         </Link>
+
+        {/* 검색창 */}
+        <div className="flex flex-1 justify-center px-10">
+          <div className="w-full max-w-[700px]">
+            <Search />
+          </div>
+        </div>
+
         <div className="flex flex-row gap-1">
           <Link href={'/login'}>
             <Image src={'/login-button.png'} alt="login" width={80} height={0} />
