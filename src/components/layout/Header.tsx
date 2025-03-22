3ee1c9bed3c0f5fd47a2d42dac4d48e8e845788e
Handsol <imhandsol@gmail.com>
2025-03-23 01:40:16 +0900

    feat #9 - 로그인 시 로그아웃 버튼으로 바뀌도록 로직 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,27 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import Search from '../Search';
+import { useEffect, useState } from 'react';
+import { isLoggedIn, logout } from '@/libs/api/auth';
 
 const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const login = await isLoggedIn();
+      setLoggedIn(login);
+    };
+    checkLogin();
+  }, []);
+
+  const handleLogout = async () => {
+    await logout();
+    setLoggedIn(false);
+  };
+
   return (
     <div className="flex flex-col items-center w-full border-b border-lightgray">
       <div className="flex flex-row w-full max-w-[1400px] h-[100px] justify-between items-center px-4">
@@ -20,9 +39,16 @@ const Header = () => {
 
         {/* 버튼 모음 */}
         <div className="flex flex-row gap-1">
-          <Link href={'/login'}>
-            <Image src={'/login-button.png'} alt="login" width={80} height={0} />
-          </Link>
+          {loggedIn ? (
+            <button onClick={handleLogout}>
+              <Image src={'/logout-button.png'} alt="logout" width={80} height={0} />
+            </button>
+          ) : (
+            <Link href={'/login'}>
+              <Image src={'/login-button.png'} alt="login" width={80} height={0} />
+            </Link>
+          )}
+
           <Link href={'/my-page'}>
             <Image src={'/mypage-button.png'} alt="login" width={80} height={0} />
           </Link>
