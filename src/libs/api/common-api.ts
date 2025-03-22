be0b68a3272f759311eb78464d1431b39bd25394
lee kiri <mango.man.e.ya@gmail.com>
2025-03-22 23:10:49 +0900

    fix#4 제품 정보 불러오기 함수에서 필드 이름을 product_id로 변경

--- src/libs/api/common-api.ts
+++ src/libs/api/common-api.ts
@@ -4,7 +4,7 @@ import { createClient } from '@/utils/supabase/create-client';
 const supabase = createClient();
 
 //제품 정보 불러오기
-export const getProduct = async (id: Products['id']): Promise<Products> => {
+export const getProduct = async (id: Products['product_id']): Promise<Products> => {
   const { data: product, error } = await supabase.from('products').select('*').eq('product_id', id).single();
   if (error) throw error;
   return product;
