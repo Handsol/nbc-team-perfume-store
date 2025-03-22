4ee23da94f770c99398dabf1d574e7bc50fff562
Handsol <imhandsol@gmail.com>
2025-03-22 20:57:11 +0900

    fix #9 - 헤더에 로고 및 버튼 모음 주석 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,6 +6,7 @@ const Header = () => {
   return (
     <div className="flex flex-col items-center w-full border-b border-lightgray">
       <div className="flex flex-row w-full max-w-[1400px] h-[100px] justify-between items-center px-4">
+        {/* 로고 */}
         <Link href={'/'}>
           <Image src={'/orot-logo.png'} alt="orot logo" width={150} height={0} />
         </Link>
@@ -17,6 +18,7 @@ const Header = () => {
           </div>
         </div>
 
+        {/* 버튼 모음 */}
         <div className="flex flex-row gap-1">
           <Link href={'/login'}>
             <Image src={'/login-button.png'} alt="login" width={80} height={0} />
