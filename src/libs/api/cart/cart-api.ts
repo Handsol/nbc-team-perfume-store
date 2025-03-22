9b522841a5b52df46df20e3d5e50cae433b71a73
lee kiri <mango.man.e.ya@gmail.com>
2025-03-22 23:19:24 +0900

    refactor#4 장바구니 관련 필드 이름을 user_id, product_id, cart_id로 변경

--- src/libs/api/cart/cart-api.ts
+++ src/libs/api/cart/cart-api.ts
@@ -4,7 +4,7 @@ import { createClient } from '@/utils/supabase/create-client';
 const supabase = createClient();
 
 //장바구니의 모든 아이템 리스트 불러오기
-export const getCartItemList = async (userId: CartItem['userId']): Promise<CartItem[]> => {
+export const getCartItemList = async (userId: CartItem['user_id']): Promise<CartItem[]> => {
   const { data: cartItemList, error } = await supabase.from('carts').select('*').eq('user_id', userId);
   if (error) throw error;
 
@@ -17,15 +17,15 @@ export const addCartItem = async (cartData: AddCartData): Promise<CartItem> => {
   const { data: checkItem, error: checkError } = await supabase
     .from('cart')
     .select('*')
-    .eq('user_id', cartData.userId) //유저아이디와, 제품아이디가 일치하는 데이터가 있는지 체크
-    .eq('product_id', cartData.productId)
+    .eq('user_id', cartData.user_id) //유저아이디와, 제품아이디가 일치하는 데이터가 있는지 체크
+    .eq('product_id', cartData.product_id)
     .maybeSingle();
   if (checkError) throw checkError;
 
   let resultData;
   if (checkItem) {
     //이미 존재하면 수량 업데이트
-    const newQuantity = checkItem.cart_quantity + cartData.quantity;
+    const newQuantity = checkItem.cart_quantity + cartData.cart_quantity;
     const { data, error } = await supabase
       .from('cart')
       .update({ cart_quantity: newQuantity })
@@ -52,7 +52,7 @@ export const updateCartItemQuantity = async ({
   cartId,
   quantity
 }: {
-  cartId: CartItem['cartId'];
+  cartId: CartItem['cart_id'];
   quantity: number;
 }): Promise<CartItem> => {
   const { data: updateCartItem, error } = await supabase
@@ -67,7 +67,7 @@ export const updateCartItemQuantity = async ({
 };
 
 //장바구니 아이템 삭제하기
-export const deleteCartItem = async (cartId: CartItem['cartId']): Promise<CartItem> => {
+export const deleteCartItem = async (cartId: CartItem['cart_id']): Promise<CartItem> => {
   const { data: deletedCartItem, error } = await supabase
     .from('carts')
     .delete()
@@ -80,7 +80,7 @@ export const deleteCartItem = async (cartId: CartItem['cartId']): Promise<CartIt
 };
 
 //장바구니 전체 비우기(사용자)
-export const deleteAllCart = async (userId: CartItem['userId']) => {
+export const deleteAllCart = async (userId: CartItem['user_id']) => {
   const { error } = await supabase.from('carts').delete().eq('user_id', userId);
   if (error) throw error;
 };
--- src/types/cart-items.ts
+++ src/types/cart-items.ts
@@ -2,12 +2,12 @@ import type { Products } from './products';
 
 //제품 타입을 상속받아 확장하여 CartItem 선언
 export interface CartItem extends Products {
-  cartId: string;
+  cart_id: string;
   // created_at: string;
-  productId: Products['id'];
-  quantity: number;
-  userId: string;
+  product_id: Products['product_id'];
+  cart_quantity: number;
+  user_id: string;
 }
 
 // 장바구니 추가시 필요한 타입
-export type AddCartData = Pick<CartItem, 'productId' | 'quantity' | 'userId'>;
+export type AddCartData = Pick<CartItem, 'product_id' | 'cart_quantity' | 'user_id'>;
