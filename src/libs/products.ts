4d9ee955b04221360864a728304d22bf1bc96acf
bonseung-dev <glynnregister@gmail.com>
2025-03-23 23:01:55 +0900

    fix #8 화살표 함수 수정

--- src/libs/products.ts
+++ src/libs/products.ts
@@ -1,10 +1,10 @@
 import { SupabaseClient } from '@supabase/supabase-js';
 import { Products } from '@/types/products';
 
-export async function getProducts(supabase: SupabaseClient): Promise<Products[]> {
+export const getProducts = async (supabase: SupabaseClient): Promise<Products[]> => {
   const { data, error } = await supabase.from('products').select('*');
   if (error) {
     throw error;
   }
   return data as Products[];
-}
+};
