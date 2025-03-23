38bc5239261229c376b3cb42d0a27910ed49b34a
Handsol <imhandsol@gmail.com>
2025-03-23 18:39:07 +0900

    fix #9 - page에 위치해있던 Header와 Navibar를 layout 파일로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import Header from '@/components/layout/Header';
-import Navi from '@/components/layout/Navi';
-
 const HomePage = () => {
-  return (
-    <div>
-      <Header />
-      <Navi />
-      Home
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default HomePage;
