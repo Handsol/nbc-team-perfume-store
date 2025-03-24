25b507c619cf386b490d2281b69cb5b319809a9a
Handsol <imhandsol@gmail.com>
2025-03-25 02:25:40 +0900

    feat #13 - 홈 페이지에 남성 및 여성 제품 섹션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Caroucel from '@/components/main/Caroucel';
+import ProductSection from '@/components/main/ProductSection';
+import { getProducts } from '@/libs/api/product/product-api';
+import { filterByCategory } from '@/utils/filterByCategory';
 
-const HomePage = () => {
+const HomePage = async () => {
+  const allProducts = await getProducts();
+  const manProducts = filterByCategory(allProducts, ['manEDT', 'manEDP', 'manEDC', 'manHP']);
+  const womanProducts = filterByCategory(allProducts, ['womanEDT', 'womanEDP', 'womanEDC', 'womanHP']);
+
   return (
     <div>
       <Caroucel />
+
+      <ProductSection title="For Man" products={manProducts} />
+      <ProductSection title="For Woman" products={womanProducts} />
     </div>
   );
 };
