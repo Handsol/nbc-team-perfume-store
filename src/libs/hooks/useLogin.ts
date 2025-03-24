c71b2075f9d50696aa99e75917621553f6382dba
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-24 09:58:14 +0900

    feat #10 - 로그인 담당 훅 구현

--- src/libs/hooks/useLogin.ts
+++ src/libs/hooks/useLogin.ts
@@ -1,28 +1,26 @@
 'use client';
 
 import { useState } from 'react';
-import { signup } from '@/libs/apis/supabaseUserAPI';
+import { login } from '@/libs/apis/supabaseUserAPI';
 
-export const useSignup = () => {
+export const useLogin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [nickname, setNickname] = useState('');
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
-  const handleSignup = async () => {
+  const handleLogin = async () => {
     setLoading(true);
     setError(null);
 
-    const { user, session, error } = await signup({ email, password, nickname });
+    const { user, session, error } = await login({ email, password });
 
     if (error) {
       setError(error);
     } else if (user && session) {
-      alert('회원가입 성공! 이메일을 확인해주세요.');
+      alert('로그인 성공!');
       setEmail('');
       setPassword('');
-      setNickname('');
     }
 
     setLoading(false);
@@ -33,10 +31,8 @@ export const useSignup = () => {
     setEmail,
     password,
     setPassword,
-    nickname,
-    setNickname,
     error,
     loading,
-    handleSignup
+    handleLogin
   };
 };
