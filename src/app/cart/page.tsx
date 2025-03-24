1041d56cdee76002a7c47f96801f8da11a970300
lee kiri <mango.man.e.ya@gmail.com>
2025-03-24 22:11:11 +0900

    refactor#4 페이지 구현을 확인하기 위해 테스트용 변수 처리

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -9,37 +9,37 @@ import { useEffect, useState } from 'react';
 
 const CartPage = () => {
   // 장바구니 테스트용 변수
-  // const user_id = '6b25b700-0dff-4f94-8425-462fcfb74d5d';
-  const user = useAuthStore((state) => state.user);
-  const [userId, setUserId] = useState<string>('');
-  const [queryClient] = useState(() => new QueryClient());
+  const userId = '6b25b700-0dff-4f94-8425-462fcfb74d5d';
+  // const user = useAuthStore((state) => state.user);
+  // const [userId, setUserId] = useState<string>('');
+  // const [queryClient] = useState(() => new QueryClient());
 
-  useEffect(() => {
-    if (user) {
-      setUserId(user.id);
-    }
-  }, [user]);
-  console.log(userId)
-  useEffect(() => {
-    queryClient.prefetchQuery({
-      queryKey: ['cart'],
-      queryFn: () => getCartItemList(userId)
-    });
-  }, [queryClient, userId]);
+  // useEffect(() => {
+  //   if (user) {
+  //     setUserId(user.id);
+  //   }
+  // }, [user]);
+  // console.log(userId)
+  // useEffect(() => {
+  //   queryClient.prefetchQuery({
+  //     queryKey: ['cart'],
+  //     queryFn: () => getCartItemList(userId)
+  //   });
+  // }, [queryClient, userId]);
 
-  // userId가 없는 상태
-  if (!userId) {
-    return <p>장바구니를 조회중입니다...</p>;
-  }
+  // // userId가 없는 상태
+  // if (!userId) {
+  //   return <p>장바구니를 조회중입니다...</p>;
+  // }
 
   return (
     <>
       <h1>CartPage</h1>
 
       <p>장바구니 목록</p>
-      <HydrationBoundary state={dehydrate(queryClient)}>
+      {/* <HydrationBoundary state={dehydrate(queryClient)}> */}
         <CartItemList user_id={userId} />
-      </HydrationBoundary>
+      {/* </HydrationBoundary> */}
     </>
   );
 };
