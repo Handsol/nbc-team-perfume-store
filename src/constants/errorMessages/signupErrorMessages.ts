dc7740d39c33e369e968d165f7451bdf17d306f6
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-25 11:45:33 +0900

    add #20 - 회원가입시 유효성 검사에 사용할 에러 메세지 상수 선언

---
+++ src/constants/errorMessages/signupErrorMessages.ts
@@ -0,0 +1,21 @@
+export const SIGNUP_ERROR_MESSAGES = {
+  email: {
+    required: '이메일은 필수 입력 항목입니다.',
+    invalid: '유효한 이메일 형식이 아닙니다.'
+  },
+  password: {
+    required: '비밀번호는 필수 입력 항목입니다.',
+    length: '비밀번호는 최소 8자 이상이어야 합니다.',
+    combination: '영문, 숫자, 특수문자가 포함되어야 합니다.',
+    noIdMatch: '아이디와 3자 이상 동일한 문자 금지'
+  },
+  nickname: {
+    required: '닉네임은 필수 입력 항목입니다.',
+    length: '닉네임은 2자 이상 12자 이하여야 합니다.',
+    specialChars: '닉네임은 특수문자를 포함할 수 없습니다.'
+  },
+  keyboard: {
+    capsLock: 'Capslock이 켜져 있습니다!',
+    numLock: 'Numlock이 켜져 있습니다!'
+  }
+};
