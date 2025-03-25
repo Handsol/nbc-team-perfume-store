6661305e07ab5a670c1acf16ff94ad69088299a2
lee kiri <mango.man.e.ya@gmail.com>
2025-03-25 16:22:50 +0900

    feat#16 결제 페이지에 컴포넌트 적용

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,8 +1,15 @@
+import PayMethod from '@/components/payment/PayMethod';
 import Shipping from '@/components/payment/Shipping';
 import DaumPostcodeEmbed, { useDaumPostcodePopup } from 'react-daum-postcode';
 import { postcodeScriptUrl } from 'react-daum-postcode/lib/loadPostcode';
 const PaymentPage = () => {
-  return <Shipping />;
+  return (
+    <div className="container mx-auto mt-10">
+      <h1 className="text-2xl mb-4">주문/결제</h1>
+      <Shipping />
+      <PayMethod />
+    </div>
+  );
 };
 
 export default PaymentPage;
