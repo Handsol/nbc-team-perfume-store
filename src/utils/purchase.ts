4e7d03d7d315ccb63b6cf570028d37d53d02879e
LSJ0706 <zosksldhs@gmail.com>
2025-03-27 02:27:47 +0900

    refactor #44 review 컴포넌트 분리, mutation 함수 분리

---
+++ src/utils/purchase.ts
@@ -0,0 +1,25 @@
+import { TCartItem } from '@/types/cart-items';
+
+// 결제금액 계산 함수
+export const calculateTotal = (cartItemList: TCartItem[]) => {
+  const checkedList = cartItemList.filter((cart) => cart.cart_checked);
+
+  let total = checkedList.reduce((cur, item) => {
+    return cur + item.products.product_price * item.cart_quantity;
+  }, 0);
+
+  const shippingPay = total === 0 ? 0 : 3000; //배송비
+  if (total < 50000) {
+    total += shippingPay;
+  }
+  return { total, shippingPay, checkedList };
+};
+
+export const setStatus = (cartItemList: TCartItem[], cartStatus: any) => {
+  const { total, checkedList } = calculateTotal(cartItemList);
+
+  // 체크된 제품 상태저장
+  cartStatus.setSelectedItems(checkedList);
+  // 총금액 상태저장
+  cartStatus.setSelectedTotal(total);
+};
