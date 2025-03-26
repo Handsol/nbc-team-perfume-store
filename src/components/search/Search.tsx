cb222fcb86635e118dbeee2fc5fc8a964e1d2ff2
Handsol <imhandsol@gmail.com>
2025-03-27 00:54:43 +0900

    refactor #46 - 검색 컴포넌트에서 커스텀 훅을 사용하여 코드 간소화

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,48 +1,19 @@
 'use client';
 
+import { useSearch } from '@/libs/hooks/useSearch';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { useState, useEffect } from 'react';
 
 const Search = () => {
-  const [query, setQuery] = useState('');
-  const [recentSearches, setRecentSearches] = useState<string[]>([]);
-  const [showDropdown, setShowDropdown] = useState(false);
-  const router = useRouter();
-
-  // 최근 검색어 불러오기
-  useEffect(() => {
-    const storedSearches = localStorage.getItem('recentSearches');
-    if (storedSearches) {
-      setRecentSearches(JSON.parse(storedSearches));
-    }
-  }, []);
-
-  // 검색 실행 및 최근 검색어 저장
-  const handleSearch = (searchQuery?: string) => {
-    const searchValue = searchQuery || query;
-    if (searchValue.trim() !== '') {
-      const updatedSearches = [searchValue, ...recentSearches.filter((item) => item !== searchValue)].slice(0, 5);
-      setRecentSearches(updatedSearches);
-      localStorage.setItem('recentSearches', JSON.stringify(updatedSearches));
-      router.push(`/search?query=${searchValue}`);
-    }
-  };
-
-  // 엔터 키 입력 시 검색 실행
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      handleSearch();
-      setShowDropdown(false);
-    }
-  };
-
-  // 최근 검색어 삭제
-  const removeSearchItem = (search: string) => {
-    const updatedSearches = recentSearches.filter((item) => item !== search);
-    setRecentSearches(updatedSearches);
-    localStorage.setItem('recentSearches', JSON.stringify(updatedSearches));
-  };
+  const {
+    query,
+    setQuery,
+    recentSearches,
+    showDropdown,
+    setShowDropdown,
+    handleSearch,
+    handleKeyDown,
+    removeSearchItem
+  } = useSearch();
 
   return (
     <div className="relative w-[500px]">
