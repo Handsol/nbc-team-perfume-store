f24669d44b428ca7eef64006a487c454f0a36543
lee kiri <mango.man.e.ya@gmail.com>
2025-03-26 21:39:19 +0900

    fix#35 카트 아이템 컴포넌트의 체크박스 상태 처리 버그픽스

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -34,39 +34,46 @@ const CartItem = ({ item, fixed }: Props) => {
   };
 
   return (
-    <li className="relative flex flex-wrap py-8 px-4 mb-6 items-start justify-between bg-lightgray" key={item.cart_id}>
-      <Checkbox
-        onCheckedChange={() => toggleCheck({ cartId: item.cart_id, checked: !item.cart_checked })}
-        className="flex items-start mr-4"
-      />
-  
+    <li className="relative mb-6 flex flex-wrap items-start justify-between bg-lightgray px-4 py-8" key={item.cart_id}>
+      {fixed ? (
+        <></>
+      ) : (
+        <Checkbox
+          checked={item.cart_checked}
+          onCheckedChange={() => toggleCheck({ cartId: item.cart_id, checked: item.cart_checked })}
+          className="mr-4 flex items-start"
+        />
+      )}
+
       <div className="mr-4">
         <Image src={products.product_thumbnail} alt={products.product_title} width={200} height={100} />
       </div>
-      <div className="flex flex-col min-h-10 w-96 mr-4 mt-4 justify-between">
+      <div className="min-h-10 mr-4 mt-4 flex w-96 flex-col justify-between">
         <div className="mb-8">
-          <h2 className="font-bold mb-4 ">{products.product_brand}</h2>
+          <h2 className="mb-4 font-bold">{products.product_brand}</h2>
           <h3>{products.product_title}</h3>
         </div>
       </div>
       {fixed ? (
-        <></>
+        <>
+          <div className="border-box h-9 w-16 text-center leading-8">수량: {item.cart_quantity}</div>
+        </>
       ) : (
         <>
-          <div className="flex flex-col w-32 mx-4 pt-10">
-            <div className="text-2xl font-bold mb-2 text-black">
+          <div className="mx-4 flex w-32 flex-col pt-10">
+            <div className="mb-2 text-2xl font-bold text-black">
               {total.toLocaleString()} <span className="text-base font-normal">원</span>
             </div>
             <div className="flex">
               <button
-                className="border-2 border-box w-9 h-9 "
+                className="border-box h-9 w-9 border-2"
                 onClick={() => handleQuantity(item.cart_id, item.cart_quantity, false)}
               >
                 -
               </button>
-              <div className="text-center border-2 border-box w-16 h-9 leading-8">{item.cart_quantity}</div>
+              <div className="border-box h-9 w-16 border-2 text-center leading-8">{item.cart_quantity}</div>
               <button
-                className="border-2 border-box w-9 h-9"
+                className="border-box h-9 w-9 border-2"
                 onClick={() => handleQuantity(item.cart_id, item.cart_quantity, true)}
               >
                 +
